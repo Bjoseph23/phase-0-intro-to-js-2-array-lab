ff3c3dab02087947bcd359589a8843a56f0a131b
+ <+>
2024-03-25 12:34:12 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(name="Arnold"){
+    cats.push(name)
+}
+function destructivelyPrependCat(name="Justin"){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name="Patrick"){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name="Lucy"){
+    cats.shift(name)
+}
+function appendCat(name){
+    return [...cats, name]
+}
+function prependCat(name){
+    return [name,...cats]
+}
+function removeLastCat () {
+    return cats.slice(0,2);
+}
+function removeFirstCat(name){
+    return cats.slice(1)
+}
